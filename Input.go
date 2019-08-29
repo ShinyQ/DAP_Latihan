31b57d5a69fbc9b5cd7f53a772a287880252a1a1
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2019-08-29 23:41:03 +0700

    UPDATE | Adding Solution for input and output process

--- Input.go
+++ Input.go
@@ -7,25 +7,25 @@ import "fmt"
 func main() {
 
 	var (
-		satu, dua, tiga string
-		temp            string
+		satu, dua, tiga string // Inisialisasi Variable
+		temp            string // Inisialisasi Variable
 	)
 
-	fmt.Print("Masukkan Input String : ")
-	fmt.Scanln(&satu)
+	fmt.Print("Masukkan Input String : ") // Memasukkan Input Untuk Variable satu
+	fmt.Scanln(&satu)                     // input(1)
 
-	fmt.Print("Masukkan Input String : ")
-	fmt.Scanln(&dua)
+	fmt.Print("Masukkan Input String : ") // Memasukkan Input Untuk Variable dua
+	fmt.Scanln(&dua)                      // input(2)
 
-	fmt.Print("Masukkan Input String : ")
-	fmt.Scanln(&tiga)
+	fmt.Print("Masukkan Input String : ") // Memasukkan Input Untuk Variable tiga
+	fmt.Scanln(&tiga)                     // input(3)
 
-	fmt.Println("Output awal = " + satu + " " + dua + " " + tiga)
+	fmt.Println("Output awal = " + satu + " " + dua + " " + tiga) // Output awal = 1 2 3
 
-	temp = satu
-	satu = dua
-	dua = tiga
-	tiga = temp
+	temp = satu // membuat variable temp bernilai variable satu, temp = satu, temp = 1
+	satu = dua  // membuat variable satu bernilai variable dua, satu = dua, satu = 2
+	dua = tiga  // membuat variable dua bernilai variable tiga, dua = tiga, dua = 3
+	tiga = temp // membuat variable tiga bernilai variable temp, tiga = temp, tiga = 1
 
-	fmt.Println("Output akhir = " + satu + " " + dua + " " + tiga)
+	fmt.Println("Output akhir = " + satu + " " + dua + " " + tiga) // Output akhir = 2 3 1
 }
